4b725835eba8cf3e44a693c44aacfb4ce2b0207b
danneg <dana@boxalino.com>
2020-09-08 18:38:38 +0200

    [uninstall] added uninstall steps

--- src/BoxalinoExporter.php
+++ src/BoxalinoExporter.php
@@ -1,6 +1,9 @@
 <?php
 namespace Boxalino\Exporter;
 
+use Shopware\Core\Framework\DataAbstractionLayer\EntityRepositoryInterface;
+use Shopware\Core\Framework\DataAbstractionLayer\Search\Criteria;
+use Shopware\Core\Framework\DataAbstractionLayer\Search\Filter\ContainsFilter;
 use Shopware\Core\Framework\Plugin;
 use Shopware\Core\Framework\Context;
 use Shopware\Core\Framework\Plugin\Context\ActivateContext;
@@ -21,7 +24,6 @@ class BoxalinoExporter extends Plugin
     {
     }
 
-
     public function activate(ActivateContext $context) : void
     {
     }
@@ -37,6 +39,30 @@ class BoxalinoExporter extends Plugin
     public function uninstall(UninstallContext $uninstallContext): void
     {
         parent::uninstall($uninstallContext);
+
+        if ($uninstallContext->keepUserData()) {
+            return;
+        }
+
+        $this->removeConfiguration($uninstallContext->getContext());
+    }
+
+    protected function removeConfiguration(Context $context): void
+    {
+        /** @var EntityRepositoryInterface $systemConfigRepository */
+        $systemConfigRepository = $this->container->get('system_config.repository');
+        $criteria = (new Criteria())->addFilter(new ContainsFilter('configurationKey', $this->getName() . '.config.'));
+        $idSearchResult = $systemConfigRepository->searchIds($criteria, $context);
+
+        $ids = array_map(static function ($id) {
+            return ['id' => $id];
+        }, $idSearchResult->getIds());
+
+        if ($ids === []) {
+            return;
+        }
+
+        $systemConfigRepository->delete($ids, $context);
     }
 
 }
