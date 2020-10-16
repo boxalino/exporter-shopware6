5564f7a957f664882e4e57803352f8c82affc662
danneg <dana@boxalino.com>
2020-10-16 10:27:30 +0200

    [command] isolate command to integration layer

--- src/Service/Delta/ProductStateRecognitionInterface.php
+++ src/Service/Delta/ProductStateRecognitionInterface.php
@@ -5,6 +5,10 @@ use Boxalino\Exporter\Service\StateRecognitionInterface;
 
 /**
  * Interface ProductStateRecognitionInterface
+ * 
+ * Declares the logic for a delta export (ex: product IDs to be synchronized/updated)
+ * Is a required dependency which has to be declared for the repository integration
+ * 
  * @package Boxalino\Exporter\Service\Delta
  */
 interface ProductStateRecognitionInterface extends StateRecognitionInterface
